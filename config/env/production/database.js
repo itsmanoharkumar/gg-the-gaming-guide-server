8a2eca9bb7fcb88bfcdf025d2946aab67548931a
Manohar <itsmanoharkumar@gmail.com>
2023-05-10 00:31:53 +0530

    trying another web app

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,38 @@
-module.exports = ({env}) => ({
-  connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "default"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  const connections = {
+    mysql: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('AZURE_MYSQL_HOST', 'localhost'),
+        port: env.int('AZURE_MYSQL_PORT', 3306),
+        database: env('AZURE_MYSQL_DATABASE', 'strapi'),
+        user: env('AZURE_MYSQL_USER', 'strapi'),
+        password: env('AZURE_MYSQL_PASSWORD', 'strapi'),
+        ssl: env.bool('AZURE_MYSQL_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
+        },
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
